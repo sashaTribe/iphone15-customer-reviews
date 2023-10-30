11611427f49323ebc4fbe9cd6055dcd0d5ff5636
sashaTribe <sasha555.tribe@gmail.com>
2023-10-30 16:05:30 +0000

    initialised database

---
+++ scrape_data.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+conn = sqlite3.connect('apple review table')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE amazon(customer_review TEXT)''')
+
+# Apple store
+
